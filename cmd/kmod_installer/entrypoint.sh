267e11bb4e937c14cf2745cd1a9cc06df3cc1acc
tyuchn <tyuchn@google.com>
2025-05-16 13:54:29 -0700

    Skip loadpin check for managed csi driver

--- cmd/kmod_installer/entrypoint.sh
+++ cmd/kmod_installer/entrypoint.sh
@@ -22,54 +22,66 @@ else
     LNET_PORT=988
 fi
 
-# Check if LoadPin is already disabled.
-# TODO: Remove this function once LoadPin excludes kernel modules.
-if cat /proc/cmdline | grep -q "loadpin"; then
-    echo "LoadPin has already been disabled. Moving to kmod installation."
-else
-    echo "Sleeping for 60s until the node is ready..."
-    sleep 60
-    echo "LoadPin is not disabled. Disabling LoadPin now."
+disable_loadpin_if_needed() {
+    # Check if LoadPin is already disabled.
+    # TODO: Remove this function once LoadPin excludes kernel modules.
+    if grep -q "loadpin" /proc/cmdline; then
+        echo "LoadPin has already been disabled. Moving to kmod installation."
+    else
+        echo "Sleeping for 60s until the node is ready..."
+        sleep 60
+        echo "LoadPin is not disabled. Disabling LoadPin now."
 
-    # Mount EFI partition and modify grub configuration
-    mkdir -p /mnt/disks
-    mount /dev/disk/by-label/EFI-SYSTEM /mnt/disks
-    sed -i -e 's|module.sig_enforce=0|module.sig_enforce=0 loadpin.enforce=0|g' /mnt/disks/efi/boot/grub.cfg
-    umount /mnt/disks
+        # Mount EFI partition and modify grub configuration
+        mkdir -p /mnt/disks
+        mount /dev/disk/by-label/EFI-SYSTEM /mnt/disks
+        sed -i -e 's|module.sig_enforce=0|module.sig_enforce=0 loadpin.enforce=0|g' /mnt/disks/efi/boot/grub.cfg
+        umount /mnt/disks
 
-    # Trigger reboot
-    echo 1 > /proc/sys/kernel/sysrq
-    echo b > /proc/sysrq-trigger
-fi
-
-# Install Lustre client drivers.
-
-# Verify if LNET has been initialized with a different port.
-if [ -f "/sys/module/lnet/parameters/accept_port" ]; then
-    CURRENT_PORT=$(cat /sys/module/lnet/parameters/accept_port)
-    if [ "$CURRENT_PORT" != "$LNET_PORT" ]; then
-        echo "ERROR: LNET_PORT mismatch! Expected: $LNET_PORT, but found: $CURRENT_PORT."
-        echo "Your node already has Lustre kernel modules installed with an outdated configuration."
-        echo "Please upgrade your node pool to apply the correct settings."
-        exit 1
+        # Trigger reboot
+        echo 1 > /proc/sys/kernel/sysrq
+        echo b > /proc/sysrq-trigger
     fi
+}
+
+check_lnet_port() {
+    if [ -f "/sys/module/lnet/parameters/accept_port" ]; then
+        CURRENT_PORT=$(cat /sys/module/lnet/parameters/accept_port)
+        if [ "$CURRENT_PORT" != "$LNET_PORT" ]; then
+            echo "ERROR: LNET_PORT mismatch! Expected: $LNET_PORT, but found: $CURRENT_PORT."
+            echo "Your node already has Lustre kernel modules installed with an outdated configuration."
+            echo "Please upgrade your node pool to apply the correct settings."
+            exit 1
+        fi
+    fi
+}
+
+install_lustre_client_drivers() {
+    # --gcs-bucket: Specifies the GCS bucket containing the driver packages ('cos-default').
+    # --module-version: Sets the lustre client driver version.
+    # --kernelmodulestree: Sets the path to the kernel modules directory on the host ('/host_modules').
+    # --lsb-release-path: Specifies the path to the lsb-release file on the host ('/host_etc/lsb-release').
+    # --insert-on-install: Inserts the module into the kernel after installation.
+    # --module-arg lnet.accept_port=${LNET_PORT}: This is crucial for setting the LNET port.
+    #                                     Lustre uses LNET for network communication, and this
+    #                                     parameter configures the port LNET will use. This is
+    #                                     essential for proper communication between Lustre clients
+    #                                     and servers. The default value is 988.
+    /usr/bin/cos-dkms install lustre-client-drivers \
+        --gcs-bucket=cos-default \
+        --module-version=2.14.0_p184 \
+        --kernelmodulestree=/host_modules \
+        --module-arg=lnet.accept_port=${LNET_PORT} \
+        --lsb-release-path=/host_etc/lsb-release \
+        --insert-on-install \
+        --logtostderr
+}
+
+if [ "${CHECK_LOADPIN}" = "true" ]; then
+    disable_loadpin_if_needed
+else
+    echo "Skipping LoadPin check and installing lustre kmods"
 fi
 
-# --gcs-bucket: Specifies the GCS bucket containing the driver packages ('cos-default').
-# --module-version: Sets the lustre client driver version.
-# --kernelmodulestree: Sets the path to the kernel modules directory on the host ('/host_modules').
-# --lsb-release-path: Specifies the path to the lsb-release file on the host ('/host_etc/lsb-release').
-# --insert-on-install: Inserts the module into the kernel after installation.
-# --module-arg lnet.accept_port=${LNET_PORT}: This is crucial for setting the LNET port.
-#                                     Lustre uses LNET for network communication, and this
-#                                     parameter configures the port LNET will use. This is
-#                                     essential for proper communication between Lustre clients
-#                                     and servers. The default value is 988.
-/usr/bin/cos-dkms install lustre-client-drivers \
-    --gcs-bucket=cos-default \
-    --module-version=2.14.0_p184 \
-    --kernelmodulestree=/host_modules \
-    --module-arg=lnet.accept_port=${LNET_PORT} \
-    --lsb-release-path=/host_etc/lsb-release \
-    --insert-on-install \
-    --logtostderr
+check_lnet_port
+install_lustre_client_drivers
